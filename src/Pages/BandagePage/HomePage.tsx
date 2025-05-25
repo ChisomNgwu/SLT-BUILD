3987aa58dc2b0fe48c67430ad33b1b2fc97b0ad0
Sommycodes <emmanueludengwu40@gmail.com>
2025-05-25 15:08:32 +0100

    image and icon modification

--- src/Pages/BandagePage/HomePage.tsx
+++ src/Pages/BandagePage/HomePage.tsx
@@ -5,7 +5,47 @@ import { FaRegStar } from 'react-icons/fa6';
 import Button from  "../../components/Button";
 
 import Logo from "../../../public/logo.svg";
-import Telephone from "../../assets/header/telephone.png";
+import Telephone from "../../assets/icons/telephone.png";
+import Email from "../../assets/icons/mail .png";
+import Instagram from "../../assets/icons/instagram.png";
+import Youtube from "../../assets/icons/youtube.png";   
+import Facebook from "../../assets/icons/facebook.png";
+import Twitter from "../../assets/icons/twitter.png";
+import UserAvatar from "../../assets/icons/user-avatar.png";
+import SearchIcon from "../../assets/icons/search.png";
+import MobileSearch from "../../assets/icons/mobile-search.png";
+import CartIcon from "../../assets/icons/cart.png";
+import MobileCartIcon from "../../assets/icons/mobile-cart.png";
+import LikeIcon from "../../assets/icons/like.png";
+import HamburgerIcon from "../../assets/icons/hamburger.png";
+import HeroImage1 from "../../assets/images/hero 1.png";
+import HeroImage2 from "../../assets/images/hero 2.png";
+import HeroImage3 from "../../assets/images/hero 3.png";
+import HeroImage4 from "../../assets/images/hero 4.png";
+import HeroMobileImage1 from "../../assets/images/hero mobile 1.png";
+import HeroMobileImage2 from "../../assets/images/hero mobile 2.png";
+import HeroMobileImage3 from "../../assets/images/hero mobile 3.png";   
+import HeroMobileImage4 from "../../assets/images/hero mobile 4.png";
+import BookReaderIcon from "../../assets/images/book-reader.png";
+import BookIcon from "../../assets/images/book.png";
+import SignalIcon from "../../assets/images/signal.png";
+import FeaturedCard1 from "../../assets/images/featured card 1.png";
+import FeaturedCard2 from "../../assets/images/featured card 2.png";    
+import FeaturedCard3 from "../../assets/images/featured card 3.png";
+import UserPic from "../../assets/images/user-pic.png";
+import Unsplash1 from "../../assets/images/unsplash 1.png";
+import Unsplash2 from "../../assets/images/unsplash 2.png";
+import Unsplash3 from "../../assets/images/unsplash 3.png";
+import Unsplash4 from "../../assets/images/unsplash 4.png";
+import Unsplash5 from "../../assets/images/unsplash 5.png";
+import Unsplash6 from "../../assets/images/unsplash 6.png";
+import Unsplash7 from "../../assets/images/unsplash 7.png";
+import Unsplash8 from "../../assets/images/unsplash 8.png";
+import Unsplash9 from "../../assets/images/unsplash 9.png";
+import FacebookFilledIcon from "../../assets/icons/facebook-filled.png";
+import InstagramFilledIcon from "../../assets/icons/instagram-filled.png";
+import TwitterFilledIcon from "../../assets/icons/twitter-filled.png";
+
 
 import './HomePage.css';
 import './MediaQuery.css'; 
@@ -56,7 +96,7 @@ export default function HomePage () {
                     </div>
 
                     <div className="content-box">
-                        <img src="/src/assets/icons/mail .png" alt="email" />
+                        <img src={Email} alt="email" />
                         <h6>michelle.rivera@example.com</h6>
                     </div>
                 </div>
@@ -65,10 +105,10 @@ export default function HomePage () {
                 </div>
                 <div className="utility-content ">
                     <h6> Follow Us  :</h6>
-                    <img src="/src/assets/icons/instagram .png" alt="instagram" />
-                    <img src="/src/assets/icons/youtube.png" alt="youtube" />
-                    <img src="/src/assets/icons/facebook.png" alt="facebook" />
-                    <img src="/src/assets/icons/twitter.png" alt="twitter" />
+                    <img src={Instagram} alt="instagram" />
+                    <img src={Youtube} alt="youtube" />
+                    <img src={Facebook} alt="facebook" />
+                    <img src={Twitter} alt="twitter" />
                 </div>
             </div>
 
@@ -95,20 +135,20 @@ export default function HomePage () {
                 </ul>
                 <div className="nav-items">
                     <div className="login">
-                        <img src="/src/assets/icons/user avatar.svg" alt="user-profile" />
+                        <img src={UserAvatar} alt="user-profile" />
                         <a href="/">Login / Register</a>
                     </div>
                     <div className="functions">
                         <div className='first'>
-                            <img src="/src/assets/icons/search icon.svg" alt="search" />
-                            <img src="/src/assets/icons/mobile search.png" alt="search" />
+                            <img src={SearchIcon} alt="search" />
+                            <img src={MobileSearch} alt="search" />
                         </div>
                         <div className='middle'>
-                            <img src="/src/assets/icons/cart.png" alt="cart" /><span className='count'>1</span>
-                            <img src="/src/assets/icons/mobile cart.png" alt="cart" />
+                            <img src={CartIcon} alt="cart" /><span className='count'>1</span>
+                            <img src={MobileCartIcon} alt="cart" />
                         </div>
                         <div className='last'>
-                            <img src="/src/assets/icons/like.png" alt="like" /><span className='count'>1</span>
+                            <img src={LikeIcon} alt="like" /><span className='count'>1</span>
                         </div>
                     </div>
                 </div>
@@ -117,7 +157,7 @@ export default function HomePage () {
                 <div className={`hamburger ${isMobileMenuOpen ? 'active' : ''}`}
                     onClick={toggleMobileMenu}
                 >
-                    <img src="/src/assets/icons/hamburger.png" alt="menu" />
+                    <img src={HamburgerIcon} alt="menu" />
                 </div>
             </nav>
             </header> 
@@ -126,16 +166,16 @@ export default function HomePage () {
             <main className="hero-section">
                 <div className="hero-wrapper">
                     <div className="hero-grid desktop-grid">
-                        <img src="/src/assets/images/hero 1.png" alt="furniture1" />
-                        <img src="/src/assets/images/hero 2.png" alt="furniture2" />
-                        <img src="/src/assets/images/hero 3.png" alt="furniture3" />
-                        <img src="/src/assets/images/hero 4.png" alt="furniture4" />
+                        <img src={HeroImage1} alt="furniture1" />
+                        <img src={HeroImage2} alt="furniture2" />
+                        <img src={HeroImage3} alt="furniture3" />
+                        <img src={HeroImage4} alt="furniture4" />
                     </div>
                     <div className=" mobile-grid">
-                        <img src="/src/assets/images/hero mobile 1.png" alt="furniture1" />
-                        <img src="/src/assets/images/hero mobile 2.png" alt="furniture2" />
-                        <img src="/src/assets/images/hero 3.png" alt="furniture3" />
-                        <img src="/src/assets/images/hero 4.png" alt="furniture4" />
+                        <img src={HeroMobileImage1} alt="furniture1" />
+                        <img src={HeroMobileImage2} alt="furniture2" />
+                        <img src={HeroMobileImage3} alt="furniture3" />
+                        <img src={HeroMobileImage4} alt="furniture4" />
                     </div>
                 </div>
             </main> 
@@ -192,17 +232,17 @@ export default function HomePage () {
                     </div>
                 <div className="best-services-wrapper">
                     <div className="best-services-item">
-                        <img src="/src/assets/icons/book-reader.png" alt="service 1" />
+                        <img src={BookReaderIcon} alt="service 1" />
                         <h3>Easy Wins</h3>
                         <p>Get your best looking smile now!</p>
                     </div>
                     <div className="best-services-item">
-                        <img src="/src/assets/icons/book.png" alt="service 2" />
+                        <img src={BookIcon} alt="service 2" />
                         <h3>Concrete</h3>
                         <p>Defalcate is most focused in helping you discover your most beautiful smile</p>
                     </div>
                     <div className="best-services-item">
-                        <img src="/src/assets/icons/signal.png" alt="service 3" />
+                        <img src={SignalIcon} alt="service 3" />
                         <h3>Hack Growth</h3>
                         <p>Overcame any hurdle or any other problem.</p>
                     </div>
@@ -218,13 +258,13 @@ export default function HomePage () {
                 </div>
                 <div className="featured-posts-wrapper">
                     <div className="featured-posts-card">
-                        <img src="/src/assets/images/featured card 1.png" alt="post 1" />
+                        <img src={FeaturedCard1} alt="post 1" />
                     </div>
                     <div className="featured-posts-card">
-                        <img src="/src/assets/images/featured card 2.png" alt="post 2" />
+                        <img src={FeaturedCard2} alt="post 2" />
                     </div>
                     <div className="featured-posts-card">
-                        <img src="/src/assets/images/featured card 3.png" alt="post 3" />
+                        <img src={FeaturedCard3} alt="post 3" />
                     </div>
                 </div>
             </section>
@@ -235,7 +275,7 @@ export default function HomePage () {
                     <div className="testimonial-box fluid">
                         <h3>What they say about us</h3>
                         <div className="user-card">
-                            <img src="/src/assets/images/user pic.png" alt="user-card" />
+                            <img src={UserPic} alt="user-card" />
                             <div className='ratings'>
                                 <FaStar />
                                 <FaStar />
@@ -252,15 +292,15 @@ export default function HomePage () {
                         <h6>Designer</h6>
                     </div>
                     <div className="testimonial-box  card-grid">
-                        <img src="/src/assets/images/unsplash 1.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 2.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 3.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 4.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 5.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 6.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 7.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 8.png" alt="unsplash" />
-                        <img src="/src/assets/images/unsplash 9.png" alt="unsplash" />  
+                        <img src={Unsplash1} alt="unsplash" />
+                        <img src={Unsplash2} alt="unsplash" />
+                        <img src={Unsplash3} alt="unsplash" />
+                        <img src={Unsplash4} alt="unsplash" />
+                        <img src={Unsplash5} alt="unsplash" />
+                        <img src={Unsplash6} alt="unsplash" />
+                        <img src={Unsplash7} alt="unsplash" />
+                        <img src={Unsplash8} alt="unsplash" />
+                        <img src={Unsplash9} alt="unsplash" />  
                     </div>
                 </div>
             </section>
@@ -292,12 +332,12 @@ export default function HomePage () {
                         <div className="footer-topbar-container">
                             <div className="footer-topbar-wrapper">
                                 <div className="footer-topbar-box1">
-                                    <img src="/src/assets/icons/logo.svg" alt="logo" />
+                                    <img src={Logo} alt="logo" />
                                 </div>
                                 <div className="footer-topbar-box2">
-                                    <img src="/src/assets/icons/facebook-filled.png" alt="facebook" />
-                                    <img src="/src/assets/icons/instagram filled.png" alt="instagram" />
-                                    <img src="/src/assets/icons/twitter filled .png" alt="twitter" />
+                                    <img src={FacebookFilledIcon} alt="facebook" />
+                                    <img src={InstagramFilledIcon} alt="instagram" />
+                                    <img src={TwitterFilledIcon} alt="twitter" />
                                 </div>
                             </div>
                         </div>
